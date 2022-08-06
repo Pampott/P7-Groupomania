69686dec51d9d7f203486ee1bb0076c13f4bf698
Paloma <celinipaloma@gmail.com>
2022-08-06 02:40:47 +0200

    first routes react

---
+++ front/src/pages/Home/index.jsx
@@ -0,0 +1,7 @@
+
+
+function Home() {
+  return <h1>Accueil 🏠 </h1>
+}
+
+export default Home;
---
+++ front/src/pages/Signup/index.jsx
@@ -0,0 +1,5 @@
+function SignUp() {
+    return <h1>Page d'inscription</h1>
+}
+
+export default SignUp;
